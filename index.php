97fd8d291ce15e5aef198de6edeceea90ee44725
Pea <patricia@nycga.net>
2013-02-06 14:29:42 -0500

    Removed testing code from index.php

--- index.php
+++ index.php
@@ -20,8 +20,6 @@ get_header(); ?>
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php if(function_exists('sticky_slider')) echo 'sticky_slider exists';?>
-
 					<?php get_template_part( 'content', 'index' ); ?>
 
 				<?php endwhile; ?>
